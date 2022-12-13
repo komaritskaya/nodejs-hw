c27120e99cbc65aa5ec17829f831e346a6f3e809
Nadezhda Komarickaya <nadezhda.komarickaya@ukg.com>
2022-12-12 23:57:28 -0300

    task 1 done

---
+++ task1.js
@@ -0,0 +1,12 @@
+#!/usr/bin/env node
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+  terminal: false,
+});
+
+rl.on('line', (line) => {
+    console.log(line.split('').reverse().join(''));
+})
